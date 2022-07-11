d1804644c28301885122a0fb3003ad7ad24ca0cb
Tobias Conrad <max.tobias.conrad@gmail.com>
2022-07-11 14:53:15 +0200

    read book details page

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -12,15 +12,23 @@ router.get("/books", (req, res, next) => {
       const data = {
         booksArr: booksFromDB
       };
-
       res.render("books/books-list", data);
-
     })
     .catch( (error) => {
       console.log("Error getting data from DB", error)
     })
-
 });
+
+router.get("/books/:bookId", (req, res) => {
+  const bookId = req.params.bookId
+  Book.findById(bookId)
+  .then( (bookById) => {
+    console.log(bookById);
+    res.render("books/books-details", bookById)
+  } )
+
+
+})
 
 
 module.exports = router;
