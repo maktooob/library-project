bb53be70ccdc86a302d854845e48fdceeb256212
Tobias Conrad <max.tobias.conrad@gmail.com>
2022-07-11 15:46:53 +0200

    create book feature

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -3,32 +3,63 @@ const Book = require("../models/Book.model");
 const router = require("express").Router();
 
 
+// READ: List all books
 router.get("/books", (req, res, next) => {
-
   Book.find()
     .then( (booksFromDB) => {
-      console.log("Query to DB worked...", booksFromDB.length)
-
       const data = {
         booksArr: booksFromDB
       };
       res.render("books/books-list", data);
     })
     .catch( (error) => {
-      console.log("Error getting data from DB", error)
+      console.log("Error getting data from DB", error);
+      next(error);
     })
 });
 
-router.get("/books/:bookId", (req, res) => {
-  const bookId = req.params.bookId
-  Book.findById(bookId)
-  .then( (bookById) => {
-    console.log(bookById);
-    res.render("books/books-details", bookById)
-  } )
 
+// CREATE: Render form
+router.get("/books/create", (req, res) => {
+  res.render("books/book-create");
+})
+
+// CREATE: Process form
+router.post("/books/create", (req, res) => {
+
+  const bookDetails = {
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    rating: req.body.rating,
+  };
+
+  Book.create(bookDetails)
+    .then( () => {
+      res.redirect("/books");
+    })
+    .catch( (error) => {
+      console.log("Error creating book in the DB", error);
+      next(error);
+    })
+})
+
+// READ: Book details
+router.get("/books/:bookId", (req, res) => {
+  const bookId = req.params.bookId;
+
+  Book.findById(bookId)
+    .then( (bookDetails) => {
+      res.render("books/book-details", bookDetails);
+    })
+    .catch( (error) => {
+      console.log("Error getting book details from DB", error);
+      next(error);
+    })
 
 })
+
+
 
 
 module.exports = router;
